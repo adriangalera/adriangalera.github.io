257fc4da40ed426f4d0004880c4cc7bc11b384a1
Adrian Galera <adrian.galera.87@gmail.com>
2024-01-09 17:17:35 +0100

    Updates

---
+++ _astro/hoisted.cbbGFmQU.js
@@ -0,0 +1,1 @@
+import{a as t}from"./hoisted.0783Gbyj.js";import"./hoisted.mpeZ8PEc.js";const o=localStorage.getItem("animations")==="true";o&&t(".title",{y:[80,0],opacity:[0,1]},{duration:2.5,opacity:{duration:3},offset:[0,.55,.75]});
