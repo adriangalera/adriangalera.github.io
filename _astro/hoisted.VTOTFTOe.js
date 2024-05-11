f7e133c2a9545f6896f9170d5c8b03b656317728
Adrian Galera <adrian.galera.87@gmail.com>
2024-05-11 14:25:31 +0200

    Updates

--- _astro/hoisted.VTOTFTOe.js
+++
@@ -1,1 +0,0 @@
-import{a as t}from"./hoisted.0DN-vXEa.js";import"./hoisted.T95ECyV2.js";const o=localStorage.getItem("animations")==="true";o&&t(".title",{y:[80,0],opacity:[0,1]},{duration:2.5,opacity:{duration:3},offset:[0,.55,.75]});
