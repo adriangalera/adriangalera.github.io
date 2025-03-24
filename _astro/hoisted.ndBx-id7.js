79c878a0bdaafd536b2db610506e8bd5c2837789
Adrian Galera <adrian.galera.87@gmail.com>
2025-03-24 17:12:07 +0100

    Updates

--- _astro/hoisted.ndBx-id7.js
+++
@@ -1,1 +0,0 @@
-import{a as t}from"./hoisted.arFG8H-4.js";import"./hoisted.Liggj7Bd.js";const o=localStorage.getItem("animations")==="true";o&&t(".title",{y:[80,0],opacity:[0,1]},{duration:2.5,opacity:{duration:3},offset:[0,.55,.75]});
