bcd8b9aa2245592ba7b803bb285a3830ec0352e1
Adrian Galera <adrian.galera.87@gmail.com>
2025-04-16 12:50:55 +0200

    Updates

--- _astro/index.astro_astro_type_script_index_0_lang.BrXIBrpQ.js
+++
@@ -1,1 +0,0 @@
-import{a as t}from"./index.BMlLUDrp.js";const a=localStorage.getItem("animations")==="true";a&&t(".title",{y:[80,0],opacity:[0,1]},{duration:2.5,opacity:{duration:3},offset:[0,.55,.75]});
